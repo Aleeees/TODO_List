a550b3cf84b5607c701fb10cf597a0448b4b6248
Aleeees <strnadales2@gmail.com>
2024-01-09 19:02:56 +0100

    iuhfiuhidhfishdfhoihsdfihsdipghipufhgjhowohoihhffgohsdfoijsffoiijsdfoijsojsdoijj

--- TODO_List/App_Start/RouteConfig.cs
+++ TODO_List/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TODO_List
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ToDo", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
